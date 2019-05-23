fe4f847fe48e790d45f3122fe4540e84141bf7b8
Louis Paulet <louis.paulet.14@neoma-bs.com>
2019-05-23 17:41:28 +0200

    Rails Mister Cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'doses/new'
+  get 'doses/create'
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  # get    'cocktails',          to: 'cocktails#index'
+  # get    'cocktails/new',      to: 'cocktails#new'
+  # post   'cocktails',          to: 'cocktails#create'
+  # get    'cocktails/:id',      to: 'cocktails#show'
+  # get    'cocktails/:id/edit', to: 'cocktails#edit'
+  # put    'cocktails/:id',      to: 'cocktails#update'
+  # delete 'cocktails/:id',      to: 'cocktails#destroy'
+
+  # get    'ingredients',          to: 'ingredients#index'
+  # get    'ingredients/new',      to: 'ingredients#new'
+  # post   'ingredients',          to: 'ingredients#create'
+  # get    'ingredients/:id',      to: 'ingredients#show'
+  # get    'ingredients/:id/edit', to: 'ingredients#edit'
+  # put    'ingredients/:id',      to: 'ingredients#update'
+  # delete 'ingredients/:id',      to: 'ingredients#destroy'
 end
